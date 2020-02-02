1ac8c067782ebf92df29613388045d43794c6a77
James Hanson <james@hanson.org>
2020-02-02 12:14:23 +0100

    #130 Cleaned up DirectoryArchiverTest

--- src/test/java/org/codehaus/plexus/archiver/dir/DirectoryArchiverTest.java
+++ src/test/java/org/codehaus/plexus/archiver/dir/DirectoryArchiverTest.java
@@ -2,29 +2,13 @@ package org.codehaus.plexus.archiver.dir;
 
 import java.io.File;
 import java.io.IOException;
-import org.junit.After;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import org.junit.Before;
 
 public class DirectoryArchiverTest {
 
     private static final File CURRENT_DIR = new File(".");
     private static final File DEST_DIR = new File("target/output/testCreateDirsForSymlink");
     private static final File SYMLINK_FILE = new File(DEST_DIR, "testSymlink");
-
-    @Before
-    public void prepare()
-    {
-        SYMLINK_FILE.delete();
-        DEST_DIR.delete();
-    }
-
-    @After
-    public void cleanUp()
-    {
-        prepare();
-    }
 
     /**
      * Test case for ISSUE-130
@@ -34,10 +18,17 @@ public class DirectoryArchiverTest {
     public void testSymlinkWithParentDirectory()
         throws IOException
     {
+        removeSymlink();
         DirectoryArchiver archiver = new DirectoryArchiver();
-        archiver.addSymlink(SYMLINK_FILE.getPath(), CURRENT_DIR.getPath());
-        archiver.setDestFile(CURRENT_DIR);
+        archiver.addSymlink( SYMLINK_FILE.getPath(), CURRENT_DIR.getPath() );
+        archiver.setDestFile( CURRENT_DIR );
         archiver.execute();
-        assertTrue(SYMLINK_FILE.isDirectory());
+        removeSymlink();
+    }
+
+    private void removeSymlink()
+    {
+        SYMLINK_FILE.delete();
+        DEST_DIR.delete();
     }
 }
